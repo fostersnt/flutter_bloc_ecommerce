540f40496f59aa48eeb1e2a1c23866f577b760e2
Foster Amponsah Asante <fostersnt@gmail.com>
2024-07-23 11:56:38 +0000

    Added http request

--- lib/data/cubit/product_cubit.dart
+++ lib/data/cubit/product_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:http/http.dart' as http;
 
 part 'product_state.dart';
 
@@ -7,5 +8,18 @@ class ProductCubit extends Cubit<ProductState> {
   ProductCubit() : super(ProductState(productName: ''));
 
   void setOriginalProductName() => emit(ProductState(productName: 'Cowbell'));
-  void setFakeProductName() => emit(ProductState(productName: 'Fanta'));
+  void setFakeProductName() async {
+    try {
+      String url = 'https://fakestoreapi.com/products';
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        print('PRODUCT DATA === ${response.body}');
+      } else {
+        print('PRODUCT RESPONSE === ${response}');
+      }
+      emit(ProductState(productName: 'Fanta'));
+    } catch (e) {
+      print('PRODUCT FETCH ERROR === ${e.toString()}');
+    }
+  }
 }
